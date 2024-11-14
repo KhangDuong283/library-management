8471068fde06d330c1a1b2fae5637235742266d4
Duong Lap Khang <khangss2803@gmail.com>
2024-11-14 11:04:11 +0700

    feat: crud độc giả

--- app/controllers/docgia.controller.js
+++ app/controllers/docgia.controller.js
@@ -1,24 +1,81 @@
-exports.create = (req, res) => {
-    res.send({ message: "Create new Độc giả" });
-}
+const ApiError = require("../api-error");
+const DocgiaService = require("../services/docgia.service");
+const MongoDB = require("../utils/mongodb.util");
 
-exports.findAll = (req, res) => {
-    res.send({ message: "Find all Độc giảs" });
-}
+// Hàm tạo mới độc giả
+exports.create = async (req, res, next) => {
+    try {
+        const docgiaService = new DocgiaService(MongoDB.client);
+        const docgia = await docgiaService.create(req.body);
+        res.status(201).send(docgia);
+    } catch (error) {
+        console.error("Lỗi khi tạo độc giả:", error);
+        next(new ApiError(500, "Không thể tạo độc giả"));
+    }
+};
 
-exports.findOne = (req, res) => {
-    res.send({ message: "Find one Độc giả" });
-}
+// Hàm tìm tất cả độc giả
+exports.findAll = async (req, res, next) => {
+    try {
+        const docgiaService = new DocgiaService(MongoDB.client);
+        const docgiaList = await docgiaService.findAll();
+        res.send(docgiaList);
+    } catch (error) {
+        console.error("Lỗi khi lấy danh sách độc giả:", error);
+        next(new ApiError(500, "Không thể lấy danh sách độc giả"));
+    }
+};
 
-exports.update = (req, res) => {
-    res.send({ message: "Update Độc giả" });
-}
+// Hàm tìm một độc giả theo ID
+exports.findOne = async (req, res, next) => {
+    try {
+        const docgiaService = new DocgiaService(MongoDB.client);
+        const docgia = await docgiaService.findById(req.params.id);
+        if (!docgia) {
+            return next(new ApiError(404, "Không tìm thấy độc giả"));
+        }
+        res.send(docgia);
+    } catch (error) {
+        next(new ApiError(500, "Lỗi khi tìm độc giả"));
+    }
+};
 
-exports.delete = (req, res) => {
-    res.send({ message: "Delete Độc giả" });
-}
+// Hàm cập nhật thông tin độc giả theo ID
+exports.update = async (req, res, next) => {
+    try {
+        const docgiaService = new DocgiaService(MongoDB.client);
+        const docgia = await docgiaService.update(req.params.id, req.body);
+        if (!docgia) {
+            return next(new ApiError(404, "Không tìm thấy độc giả để cập nhật"));
+        }
+        res.send(docgia);
+    } catch (error) {
+        console.error("Lỗi khi cập nhật độc giả:", error);
+        next(new ApiError(500, "Không thể cập nhật độc giả"));
+    }
+};
 
-exports.deleteAll = (req, res) => {
-    res.send({ message: "Delete all Độc giảs" });
-}
+// Hàm xóa một độc giả theo ID
+exports.delete = async (req, res, next) => {
+    try {
+        const docgiaService = new DocgiaService(MongoDB.client);
+        const docgia = await docgiaService.delete(req.params.id);
+        if (!docgia) {
+            return next(new ApiError(404, "Không tìm thấy độc giả để xóa"));
+        }
+        res.send({ message: "Xóa độc giả thành công" });
+    } catch (error) {
+        next(new ApiError(500, "Không thể xóa độc giả"));
+    }
+};
 
+// Hàm xóa tất cả độc giả
+exports.deleteAll = async (req, res, next) => {
+    try {
+        const docgiaService = new DocgiaService(MongoDB.client);
+        await docgiaService.deleteAll();
+        res.send({ message: "Xóa tất cả độc giả thành công" });
+    } catch (error) {
+        next(new ApiError(500, "Không thể xóa tất cả độc giả"));
+    }
+};
